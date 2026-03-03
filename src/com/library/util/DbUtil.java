d1d0f3812ccf6ff2281b50876b4ba0981d54f260
Roshani231 <roshanidangat9@gmail.com>
2026-03-03 18:02:12 +0530

    Fix #1: hide DB credentials using config.properties

--- src/com/library/util/DbUtil.java
+++ src/com/library/util/DbUtil.java
@@ -1,26 +1,42 @@
 package com.library.util;
 
+
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class DbUtil {
 
-    private static final String URL = "jdbc:mysql://localhost:3306/library_db";
-    private static final String USER = "root";
-    private static final String PASSWORD = "Ruhii@0411";
+    private static final String URL;
+    private static final String USER;
+    private static final String PASSWORD;
 
     static {
-        try {
+        try (InputStream is = DbUtil.class.getClassLoader()
+                                          .getResourceAsStream("config.properties")) {
+
+            if (is == null) {
+                throw new RuntimeException("config.properties not found in src/");
+            }
+
+            Properties props = new Properties();
+            props.load(is);
+
+            URL      = props.getProperty("db.url");
+            USER     = props.getProperty("db.user");
+            PASSWORD = props.getProperty("db.password");
+
             Class.forName("com.mysql.cj.jdbc.Driver");
-        } catch (ClassNotFoundException e) {
-            throw new RuntimeException("MySQL Driver not found", e);
+
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to load DB config: " + e.getMessage(), e);
         }
     }
 
-    private DbUtil() {
-        // prevent object creation
-    }
+    // Prevent object creation
+    private DbUtil() {}
 
     public static Connection getConnection() {
         try {
